4ebe37c1c1b5ad659f8c933ed9adcba4647bda36
Juakoo2103 <joaquineduardo.gonzalez@gmail.com>
2024-10-09 19:59:00 -0300

    desarrollo finalizado

---
+++ src/components/Componente1JG.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <img :src="imageSrc" alt="Img" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        imageSrc: 'https://i.pinimg.com/474x/78/7d/d9/787dd92efa53d2a0c355d4a47a91850a.jpg'
+      };
+    },
+  };
+  </script>
+  
+
+
+<!-- 7.-  Enlace dinámico al atributo src de una imagen
+Enunciado: Crea un componente que muestre una imagen con un src dinámico. El enlace de la imagen debe estar almacenado en el data del componente y cambiarse mediante v-bind.
+ -->
---
+++ src/components/Componente2JG.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <button 
+        :class="{'active-class': isActive}" 
+        @click="isActive = !isActive">
+        Cambiar Clase
+      </button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isActive: false // Estado inicial del botón
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .active-class {
+    background-color: green;
+    color: white;
+  }
+  </style>
+  
